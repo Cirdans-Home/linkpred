94cacba0a1b33cc3e2da238a6ff201fcbe461c1d
cirdans-home <f.durastante@na.iac.cnr.it>
2020-05-29 17:13:56 +0200

    Modified to run with various c values

--- NonLocalPageRankExamples/NonLocalPageRankExample.py
+++ NonLocalPageRankExamples/NonLocalPageRankExample.py
@@ -13,11 +13,11 @@ import random
 import networkx as nx
 from matplotlib import pyplot as plt
 from sklearn.metrics import auc
-from numpy import floor, logspace, array
+from numpy import floor, logspace, array, linspace
 
 random.seed(100)
 
-for networkname in ["USAir97","netscience","CSphd","odlis"]:
+for networkname in ["netscience"]:
     # Read network
     G = linkpred.read_network('NonLocalPageRankExamples/'+networkname+'.net')
     
@@ -29,7 +29,7 @@ for networkname in ["USAir97","netscience","CSphd","odlis"]:
         d['weight']=1
     
     # Create test network
-    test = G.subgraph(random.sample(G.nodes(), floor(0.90*G.number_of_nodes()).astype(int) )).copy()
+    test = G.subgraph(random.sample(G.nodes(), floor(0.50*G.number_of_nodes()).astype(int) )).copy()
     
     # Exclude test network from learning phase
     training = G.copy()
@@ -49,13 +49,21 @@ for networkname in ["USAir97","netscience","CSphd","odlis"]:
     plt.ylabel('Precision')
     ax.loglog(evaluationbaseline.recall(), evaluationbaseline.precision(),label='Baseline')
     
+     
+    # Save the AUC to file
+    outfile = open("linkpredlog.txt","a+")
+    outfile.write(networkname+':\n')
+    outfile.write('Test Edges : '+str(test.number_of_edges())+' Training edges: '+str(training.number_of_edges())+'\n')
+    outfile.write("Baseline AUC "+str(aucbaseline)+"\n")
+    
     ## LinkPredicition with the Local (Rooted) PageRank
     rootpr = linkpred.predictors.RootedPageRank(training, excluded=training.edges())
-    rootpr_results = rootpr.predict()
-    
     test_set = set(linkpred.evaluation.Pair(u, v) for u, v in test.edges())
-    evaluationpr = linkpred.evaluation.EvaluationSheet(rootpr_results, test_set)
-    auclocal = auc(evaluationpr.recall(), evaluationpr.precision())
+    for cvalue in linspace(0.1,0.9,18):
+        rootpr_results = rootpr.predict(alpha=cvalue)
+        evaluationpr = linkpred.evaluation.EvaluationSheet(rootpr_results, test_set)
+        auclocal = auc(evaluationpr.recall(), evaluationpr.precision())
+        outfile.write("PageRank c ="+str(cvalue)+" AUC "+str(auclocal)+"\n")
     
     ## Plotting the Precision/Recall curves
     ax.plot(evaluationpr.recall(), evaluationpr.precision(),label='PageRank')
@@ -67,29 +75,19 @@ for networkname in ["USAir97","netscience","CSphd","odlis"]:
     # work on a neighboroud of the node, then the precomputation is unnecessary,
     # because it is then done on the fly on the reduced graph. 
     W = nx.floyd_warshall_numpy(training) 
-    index = 0
-    aucnonlocal = array([0.,1.,2.,3.,4.,5.])
-    for gamma in logspace(-2.0, 1.0, num=6, endpoint=True): # Coefficient of the NonLocality
-        rootnonlocal = linkpred.predictors.NonLocalPageRank(training, DistanceMatrix=W, excluded=training.edges())
-        rootnonlocal_results = rootnonlocal.predict(gamma = gamma)
-        test_set = set(linkpred.evaluation.Pair(u, v) for u, v in test.edges())
-        evaluation_nonlocal = linkpred.evaluation.EvaluationSheet(rootnonlocal_results, test_set)
-        aucnonlocal[index] = auc(evaluation_nonlocal.recall(), evaluation_nonlocal.precision())
-        index = index+1
-        ax.loglog(evaluation_nonlocal.recall(), evaluation_nonlocal.precision(),label='NonLocal PageRank alpha='+str(gamma))
-    
-    
+    rootnonlocal = linkpred.predictors.NonLocalPageRank(training, DistanceMatrix=W, excluded=training.edges())
+    for cvalue in linspace(0.1,0.9,18):
+        for gamma in logspace(-2.0, 1.0, num=6, endpoint=True): # Coefficient of the NonLocality
+            rootnonlocal_results = rootnonlocal.predict(gamma = gamma, alpha = cvalue)
+            test_set = set(linkpred.evaluation.Pair(u, v) for u, v in test.edges())
+            evaluation_nonlocal = linkpred.evaluation.EvaluationSheet(rootnonlocal_results, test_set)
+            aucnonlocal = auc(evaluation_nonlocal.recall(), evaluation_nonlocal.precision())
+            ax.loglog(evaluation_nonlocal.recall(), evaluation_nonlocal.precision(),label='NonLocal PageRank alpha='+str(gamma)+' c = '+str(cvalue))
+            outfile.write("NonLocal PageRank "+str(gamma)+" c = "+str(cvalue)+" AUC "+str(aucnonlocal)+"\n")
+
+    # Close the file, it will be reopen for next network            
+    outfile.close()
+
     # Plot of the results
     leg = ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., frameon=False);
     fig.savefig(networkname+'.eps', bbox_extra_artists=(leg,), bbox_inches='tight', format='eps')
-    
-    # Save the AUC to file
-    outfile = open("linkpredlog.txt","a+")
-    outfile.write(networkname+':\n')
-    outfile.write("Baseline AUC "+str(aucbaseline)+"\n")
-    outfile.write("PageRank AUC "+str(auclocal)+"\n")
-    index = 0
-    for gamma in logspace(-2.0, 1.0, num=6, endpoint=True):
-        outfile.write("NonLocal PageRank "+str(gamma)+" AUC "+str(aucnonlocal[index])+"\n")
-        index=index+1
-    outfile.close()
